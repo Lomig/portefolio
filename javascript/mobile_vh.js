91b819e4ffa60ef4bbf844cb01ddf14137d759d8
Lomig <dunwael@gmail.com>
2020-03-16 15:44:40 +0100

    Remove debug logs

--- javascript/mobile_vh.js
+++ javascript/mobile_vh.js
@@ -1,6 +1,5 @@
 const updateHeight = () => {
   const vh = window.innerHeight * 0.01;
-  console.log(vh);
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 
   window.addEventListener("resize", updateHeight);
